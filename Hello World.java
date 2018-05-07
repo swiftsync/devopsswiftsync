432b6f6aaf99827087390b5c73e22a2129d0345a
swiftsync <swiftsynctest@gmail.com>
2018-05-06 23:21:34 -0700

    IssueId::638744 entityType::KanbanDefect Reviewer::admin Status::Done Comments::test

--- Hello World.java
+++ Hello World.java
@@ -1,7 +1,7 @@
 public class HelloWorld
 {
 	public static void main(String[] args) {
-		 System.out.println("FIxed is issue at Devops update3");
+		 System.out.println("FIxed is issue at Devops");
 		 System Update!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	}
 }
