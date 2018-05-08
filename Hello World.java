f98eb284d7fe14c10a95f093e52ed07884914a90
swiftsync <swiftsynctest@gmail.com>
2018-05-08 03:15:23 -0700

    IssueId::638882 entityType::KanbanDefect Reviewer::admin Status::Done Comments::DEF3 update from Git 2nd Update

--- Hello World.java
+++ Hello World.java
@@ -1,7 +1,7 @@
 public class HelloWorld
 {
 	public static void main(String[] args) {
-		 System.out.println("FIxed is issue at card Update DEF3");
+		 System.out.println("FIxed is issue at card Update DEF3 2nd updte");
 		 System Update!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	}
 }
