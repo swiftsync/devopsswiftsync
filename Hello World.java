8afecba8446383d7f511629af20151c9073f4988
swiftsync <swiftsynctest@gmail.com>
2018-05-09 04:11:16 -0700

    IssueId::638974 entityType::KanbanDefect Reviewer::admin Status::Done Comments::DEF11 Add commit from Git 2n

--- Hello World.java
+++ Hello World.java
@@ -1,7 +1,7 @@
 public class HelloWorld
 {
 	public static void main(String[] args) {
-		 System.out.println("FIxed is issue at card Update DEF10 2update");
+		 System.out.println("FIxed is issue at card Update DEF11");
 		 System Update!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	}
 }
