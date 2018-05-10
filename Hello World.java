535e057fb953ba4e28230fd577034c228d75eff1
swiftsyncdemo <swiftsync2017@gmail.com>
2018-05-10 14:33:10 +0530

    IssueId::639055 entityType::KanbanDefect Reviewer::admin Status::Done Comments::DEF17 upgrdae from Niket 2n Upgrade

--- Hello World.java
+++ Hello World.java
@@ -1,7 +1,7 @@
 public class HelloWorld
 {
 	public static void main(String[] args) {
-		 System.out.println("FIxed is issue at card Update DEF16");
+		 System.out.println("FIxed is issue at card Update DEF17 2nd Upgrade");
 		 System Update!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	}
 }
