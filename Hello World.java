8c0cf82a0c7d1f7535831191d98ded2b084f10ba
swiftsync <swiftsynctest@gmail.com>
2018-05-11 11:33:17 +0530

    IssueId::639134 entityType::DEF Reviewer::admin Status::Done Comments::DEF21 upgrdae from Niket

--- Hello World.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld
-{
-	publi static void main(String[] args) {
-		 System.out.println("FIxed is issue at card Update DEF19 update2");
-		 System Update!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	}
-}
